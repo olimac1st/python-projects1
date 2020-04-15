3639fd0e2673b85d435277450dfd97136165b54e
olimac1st <olimac1@st.amu.edu.pl>
2020-04-15 15:15:42 +0200

    Implemented fetching the whole document

--- webcrawler.py
+++ webcrawler.py
@@ -6,3 +6,14 @@ Created on Wed Apr 15 15:08:10 2020
 """
 
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+print(doc)
