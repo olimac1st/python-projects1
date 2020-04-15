e77bd4422ecef5573a2885fe6567c4b15ac24a1f
olimac1st <olimac1@st.amu.edu.pl>
2020-04-15 17:05:29 +0200

    fetch all link elements

--- webcrawler.py
+++ webcrawler.py
@@ -17,4 +17,9 @@ data = response.read()
 doc = BeautifulSoup(data, 'html.parser')
 
 staff_content = doc.find(id='tresc_wlasciwa')
-print(staff_content)
+
+links = staff_content.find_all('a')
+
+print(links)
+
+
