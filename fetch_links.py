ac61b9fedbb3fd0cb3fdcb4749329bb24a56d1e6
olimac1st <olimac1@st.amu.edu.pl>
2020-06-11 13:59:41 +0200

    fetch_links.py

---
+++ fetch_links.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 13:41:02 2020
+
+@author: Olivia
+"""
+
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = input("Input your url:")
+#url = 'file:///C:/Users/Olivia/Desktop/exp1.html'
+response = urllib.request.urlopen(url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+lists = doc.find_all('ol')
+for li in lists:
+    i = 1
+    list_items = li.find_all('li')
+    print('\n')
+    for l in list_items:
+        print(str(i) +'. ' + l.get_text())
+        i += 1
+        
+        
