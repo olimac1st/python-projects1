4e2ef9517f774784682ba680b09160d8b87a9a05
olimac1st <olimac1@st.amu.edu.pl>
2020-06-11 14:34:14 +0200

    implemented translate library

--- translate.py
+++ translate.py
@@ -6,3 +6,12 @@ Created on Thu Jun 11 14:22:29 2020
 """
 
 
+from googletrans import Translator
+
+
+def main():
+    translator = Translator()
+    result = translator.translate('Jak się masz?')
+    print(result.text)
+    
+main()
